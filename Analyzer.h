17204331546ad54f459e167b83a1ea2136f897e4
Chloe Moore <chloemoore715@gmail.com>
2025-03-12 15:40:46 -0500

    Create Analyzer.h

---
+++ Analyzer.h
@@ -0,0 +1,23 @@
+#ifndef SENTIMENTANALYZER_H
+#define SENTIMENTANALYZER_H
+
+# include <map>
+# include "DSString.h"
+#include <map>
+using namespace std;
+
+class SentimentAnalyzer {
+public:
+    //to train
+    void train(char*);
+    //to test
+    void test(char*, char*, char*, char*);
+
+private:
+    //initialize
+   map<DSString, int> dictionary;
+   double percentage;
+   double accuracyCounter;
+   double tweetsClassified; 
+};
+#endif
